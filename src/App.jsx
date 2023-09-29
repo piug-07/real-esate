49829887a989ea76b01b56d2623635b4c4618fcb
Piyush Gupta <guptapiyush315@gmail.com>
2023-09-29 23:29:06 +0530

    backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
-// import Header from './components/Header';
+import Header from './components/Header';
 // import PrivateRoute from './components/PrivateRoute';
 // import CreateListing from './pages/CreateListing';
 // import UpdateListing from './pages/UpdateListing';
@@ -15,7 +15,7 @@ import Profile from './pages/Profile';
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
