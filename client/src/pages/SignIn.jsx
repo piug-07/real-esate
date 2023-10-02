537f3e0f135cca6fdc614d0f8e0622ba5aa00cb6
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-02 16:39:04 +0530

    redux-tool

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-    // const { loading, error } = useSelector((state) => state.user);
+    const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
 
 
   const handleChange = (e) => {
@@ -21,7 +25,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // dispatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -33,13 +37,13 @@ const SignIn = () => {
       console.log(data);
       
       if (data.success === false) {
-        // dispatch(signInFailure(data.message));
+        dispatch(signInFailure(data.message));
         return;
       }
-      // dispatch(signInSuccess(data));
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      // dispatch(signInFailure(error.message));
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -64,11 +68,10 @@ const SignIn = () => {
         />
 
         <button
-          // disabled={loading}
+          disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {/* {loading ? 'Loading...' : 'Sign In'} */}
-          sign
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
         {/* <OAuth/> */}
       </form>
@@ -78,7 +81,7 @@ const SignIn = () => {
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
-      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
     )
   }
