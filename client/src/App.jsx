342ff692754faafd835c5f815c0c4301fcd266ec
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-03 19:41:57 +0530

    protectedroute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-// import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute';
 // import CreateListing from './pages/CreateListing';
 // import UpdateListing from './pages/UpdateListing';
 // import Listing from './pages/Listing';
@@ -24,14 +24,14 @@ function App() {
         {/* <Route path='/search' element={<Search />} /> */}
         {/* <Route path='/listing/:listingId' element={<Listing />} /> */}
 
-        {/* <Route element={<PrivateRoute />}> */}
+        <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
         {/* <Route path='/create-listing' element={<CreateListing />} /> */}
         {/* <Route
           path='/update-listing/:listingId'
           element={<UpdateListing />}
         /> */}
-        {/* </Route> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
