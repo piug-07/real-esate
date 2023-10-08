cda3caf16c0ae6e46ebf34934e28ea7bf9ce651a
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-08 10:41:47 +0530

    listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 
 export const test = (req, res) => {
@@ -48,5 +49,33 @@ export const updateUser = async (req, res, next) => {
       next(error);
     }
   };
+
+
+  export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+  }; 
+
+
+  export const getUser = async (req, res, next) => {
+    try {
+      const user = await User.findById(req.params.id);
+
+      if (!user) return next(errorHandler(404, 'User not found!'));
+
+      const { password, ...rest } = user._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
 
 
