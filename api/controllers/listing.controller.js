fc6ee3a81343465e84f451b6e4d30e11fcb609ff
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-09 19:41:20 +0530

    list

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -6,9 +6,29 @@ export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
-        
+
     } catch (error) {
         next(error);
-    } 
+    }
 
-}  
+}
+
+
+export const deleteListing = async (req, res, next) => {
+
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(errorHandler(404, 'Listing not found!'));
+
+
+    if (listing.userRef.toString() !== req.user.id)
+        return next(errorHandler(401, 'You can only delete your own listings!'));
+
+        
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('Listing has been deleted!');
+    } catch (error) {
+        next(error);
+    }
+
+};
