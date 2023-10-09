ccadbe969f4f21aaeef71b1df09a031a2bcaf245
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-09 22:39:18 +0530

    updating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
-// import UpdateListing from './pages/UpdateListing';
+import UpdateListing from './pages/UpdateListing';
 // import Listing from './pages/Listing';
 // import Search from './pages/Search';
 
@@ -27,10 +27,10 @@ function App() {
         <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
         <Route path='/create-listing' element={<CreateListing />} />
-        {/* <Route
+        <Route
           path='/update-listing/:listingId'
           element={<UpdateListing />}
-        /> */}
+        />
         </Route>
       </Routes>
     </BrowserRouter>
