ff74e4f3846c47cf0c636f2f8680e7dbb21708a1
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-12 22:04:07 +0530

    final

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 
 
@@ -18,6 +20,7 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
 
   const app = express();
   app.use(cookieParser());
@@ -33,6 +36,12 @@ app.listen(3000, () => {
   app.use('/api/auth', authRouter);
   app.use('/api/listing', listingRouter);
 
+
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
   app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
